25a88e3e073c5e687c3beb46823771a2c5de38da
victor boddie <victorboddie21@gmail.com>
2022-11-12 16:22:10 -0500

    finished model routes added user routes, created routes and api folder to being testing connections.

--- models/Users.js
+++ models/Users.js
@@ -1,2 +1,58 @@
 const { Schema, model } = require("mongoose");
 
+const UserSchema = new Schema(
+
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /.+\@.+\..+/
+
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+                
+            }
+
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        
+        ],
+
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+
+        },
+
+    }
+
+
+
+);
+
+
+
+UserSchema.virtuals('friendCount').get(function () {
+    return this.friends.length
+});
+
