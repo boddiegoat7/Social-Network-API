3b6dc1233e5a128ad7efb7cd09734b95273ea560
victor boddie <victorboddie21@gmail.com>
2022-11-12 15:16:08 -0500

    added controlloer folder and controller for the thoughts model

---
+++ controllers/thoughts-controller.js
@@ -0,0 +1,9 @@
+const { Thoughts } = require("../models");
+
+const ThoughtsController = {
+  // the functions will go in here as methods
+};
+
+
+
+module.exports = ThoughtsController;
--- models/Thoughts.js
+++ models/Thoughts.js
@@ -29,11 +29,65 @@ const ReactionSchema = new Schema({
     },
 
 },
-    
-    
+
+    {
+        toJSON: {
+            getters: true
+        
+        }
+    }
+
 );
 
-    
+
+
+const Thoughtschema = new Schema(
+
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280
+        
+        },
+
+        createdAt: {
+            type: date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+            
+        },
+
+        username: {
+            type: String,
+            required: true,
+
+        },
+
+        reaction: [ReactionSchema]
+        
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+
+    }
+
+
+);
+
+Thoughtschema.virtual(`reactionCount`).get(function () {
+    return this.reactions.legth;
+
+});
+
+
+const Though = model(`Thoughts`, ThoughtsSchema);
+
+
 module.exports = Thoughs;
 
 
