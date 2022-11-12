995b556c52b56891a048f1d586893f5be58d087e
victor boddie <victorboddie21@gmail.com>
2022-11-12 16:37:53 -0500

    deleted duplicate controller folders that caused and error linking paths

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,9 +1,23 @@
 const router = require("express").Router();
 
-// Set up GET all and POST at /api/pizzas
+const {
+    addThought,
+    getAllThoughts,
+    getThoughtById,
+    updateThought,
+    removeThought,
+    addReaction,
+    deleteReaction,
+
+} = require("../../controllers/thoughts-controller");
+
+
+
+
+// Set up GET all and POST at /api/thoughts
 router.route("/").get().post();
 
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+// Set up GET one, PUT, and DELETE at /api/thoughts/:id
 router.route("/:id")
     
     
